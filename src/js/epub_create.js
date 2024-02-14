a70fb7f2453cf9068b2a3a2e0264aea10544de0b
t.m <takashi.orion.999.428@gmail.com>
2024-02-15 01:33:39 +0900

    archiverを使ってコンパイル(成功せず)

--- src/js/epub_create.js
+++ src/js/epub_create.js
@@ -2,23 +2,65 @@ import { STYLE_CSS } from "./statics.js";
 const fs = require("fs");
 const path = require("path");
 const builder = require("xmlbuilder");
+const archiver = require("archiver");
 
 export async function create_epub(epub) {
     await create_folder(epub);
     if (epub.working_folder == undefined) {
         return;
     }
-    create_mimetype(epub);
-    create_conteiner_xml(epub);
+    // create_mimetype(epub);
+    await create_conteiner_xml(epub);
     await image_copy(epub);
-    create_style_css(epub);
-    create_standard_opf(epub);
-    // debug
-    for (let file of epub.files) {
-        console.log(file);
+    await create_style_css(epub);
+    await create_navigation_documents(epub);
+    await create_standard_opf(epub);
+    // compile
+    await create_epub_file(epub);
+}
+
+function append_file(archive, working_folder, folder_path) {
+    const files = fs.readdirSync(working_folder + "/" + folder_path);
+    for (let file of files) {
+        const file_path = working_folder + "/" + folder_path + "/" + file;
+        if (fs.lstatSync(file_path).isDirectory()) {
+            console.log("folder: " + folder_path + "/" + file);
+            append_file(archive, working_folder, folder_path + "/" + file);
+        } else {
+            archive.append(fs.readFileSync(file_path), {
+                name: folder_path + "/" + file,
+            });
+            console.log("append: " + folder_path + "/" + file);
+        }
     }
 }
 
+async function create_epub_file(epub) {
+    const folder_path = epub.create_folder;
+    const working_folder = folder_path + "/output";
+    const file_path = folder_path + "/" + epub.file_name;
+    let archive = archiver("zip", {
+        zlib: { level: 9 }, // Sets the compression level.
+    });
+    let output = fs.createWriteStream(file_path);
+    archive.pipe(output);
+    archive.append("application/epub+zip", {
+        name: "mimetype",
+        store: true,
+    });
+    // TODO: fix this
+    // can't use archive.directory because of the mimetype file
+    // now use archive.append instead of archive.directory
+    append_file(archive, working_folder, "META-INF");
+    append_file(archive, working_folder, "item");
+
+    archive.finalize();
+}
+
+/**
+ * delete folder recursively
+ * @param {string} directory
+ */
 function deleteFolderRecursive(directory) {
     if (fs.existsSync(directory)) {
         fs.readdirSync(directory).forEach((file) => {
@@ -36,8 +78,8 @@ function deleteFolderRecursive(directory) {
 }
 
 async function create_folder(epub) {
-    const path = epub.create_folder;
-    const working_folder = path + "/output";
+    const folder_path = epub.create_folder;
+    const working_folder = folder_path + "/output";
     epub.working_folder = working_folder;
     // make output
     if (!fs.existsSync(working_folder)) {
@@ -62,15 +104,25 @@ async function create_folder(epub) {
     fs.mkdirSync(working_folder + "/item/xhtml");
 }
 
+/**
+ * create mimetype
+ * @param {Epub} epub
+ */
+/*
 function create_mimetype(epub) {
     const file_name = "mimetype";
     let mimetype_text = "application/epub+zip";
-    const path = epub.working_folder + "/" + file_name;
-    fs.writeFileSync(path, mimetype_text);
+    const file_path = epub.working_folder + "/" + file_name;
+    fs.writeFileSync(file_path, mimetype_text);
 }
+*/
 
-function create_conteiner_xml(epub) {
-    const file_name = "conteiner.xml";
+/**
+ * create conteiner.xml
+ * @param {Epub} epub
+ */
+async function create_conteiner_xml(epub) {
+    const file_name = "container.xml";
     // make xml text
     let xml = builder.create("container");
     xml.att("version", "1.0");
@@ -80,16 +132,16 @@ function create_conteiner_xml(epub) {
     rootfile.att("full-path", "item/standard.opf");
     rootfile.att("media-type", "application/oebps-package+xml");
     // write file
-    const path = epub.working_folder + "/META-INF/" + file_name;
-    fs.writeFileSync(path, xml.end({ pretty: true }));
+    const file_path = epub.working_folder + "/META-INF/" + file_name;
+    await fs.writeFileSync(file_path, xml.end({ pretty: true }));
 }
 
 async function image_copy(epub) {
-    const path = epub.working_folder + "/item/image";
+    const folder_path = epub.working_folder + "/item/image";
     // make output
-    if (!fs.existsSync(path)) {
+    if (!fs.existsSync(folder_path)) {
         // make output folder recursively
-        fs.mkdirSync(path, { recursive: true });
+        fs.mkdirSync(folder_path, { recursive: true });
     }
     const totalDigits = String(epub.files.length).length;
     for (let i = 0; i < epub.files.length; i++) {
@@ -106,26 +158,29 @@ async function image_copy(epub) {
                     "." +
                     file.name.split(".").pop();
             }
-            fs.copyFileSync(file.path, path + "/" + file_name);
+            fs.copyFileSync(file.path, folder_path + "/" + file_name);
             file.image_path = "image/" + file_name;
             // make xhtml
             await make_xhtml_from_image(epub, file, file_name);
         } else {
             console.log("not image");
-            fs.copyFileSync(file.path, path + "/" + file.name);
+            fs.copyFileSync(file.path, folder_path + "/" + file.name);
         }
     }
 }
 
 async function make_xhtml_from_image(epub, file, file_name) {
-    const path = epub.working_folder + "/item/xhtml";
+    const folder_path = epub.working_folder + "/item/xhtml";
     // make output
-    if (!fs.existsSync(path)) {
+    if (!fs.existsSync(folder_path)) {
         // make output folder recursively
-        fs.mkdirSync(path, { recursive: true });
+        fs.mkdirSync(folder_path, { recursive: true });
     }
     // make xhtml
-    const xhtml = builder.create("html");
+    const xhtml = builder.create("html", {
+        encoding: "UTF-8",
+        doctype: "html",
+    });
     xhtml.att("xmlns", "http://www.w3.org/1999/xhtml");
     xhtml.att("lang", "ja");
     xhtml.att("xmlns:epub", "http://www.idpf.org/2007/ops");
@@ -138,9 +193,7 @@ async function make_xhtml_from_image(epub, file, file_name) {
     style.att("type", "text/css");
     style.att("href", "../css/style.css");
     style.att("rel", "stylesheet");
-    let size = head.ele("meta");
-    size.att("name", "viewport");
-    size.att("content", `width=${file.width}, height=${file.height}`);
+
     let body = xhtml.ele("body");
     let div = body.ele("div");
     div.att("class", "main");
@@ -152,21 +205,81 @@ async function make_xhtml_from_image(epub, file, file_name) {
     // image file name is i-001.jpg, xhtml file name is p-001.xhtml
     const xhtml_file_name =
         "p-" + file_name.split("-")[1].split(".")[0] + ".xhtml";
-    const xhtml_path = path + "/" + xhtml_file_name;
+    const xhtml_path = folder_path + "/" + xhtml_file_name;
     file.xhtml_path = "xhtml/" + xhtml_file_name;
-    fs.writeFileSync(xhtml_path, xhtml.end({ pretty: true }));
+
+    let size = head.ele("meta");
+    size.att("name", "viewport");
+    const img_render = new Image();
+    img_render.src = URL.createObjectURL(file);
+    img_render.onload = function () {
+        const width = this.width;
+        const height = this.height;
+        size.att("content", `width=${width}, height=${height}`);
+        fs.writeFileSync(xhtml_path, xhtml.end({ pretty: true }));
+    };
+    img_render.onerror = function () {
+        fs.writeFileSync(xhtml_path, xhtml.end({ pretty: true }));
+    };
 }
 
-function create_style_css(epub) {
+/**
+ * create style.css
+ * @param {Epub} epub
+ */
+async function create_style_css(epub) {
     const file_name = "style.css";
     // make xml text
     let css = STYLE_CSS;
     // write file
-    const path = epub.working_folder + "/item/style/" + file_name;
-    fs.writeFileSync(path, css);
+    const file_path = epub.working_folder + "/item/style/" + file_name;
+    fs.writeFileSync(file_path, css);
 }
 
-function create_standard_opf(epub) {
+async function create_navigation_documents(epub) {
+    const file_name = "navigation-documents.xhtml";
+    // make xml text
+    let xml = builder.create("html", {
+        encoding: "UTF-8",
+        doctype: "html",
+    });
+    xml.att("xmlns", "http://www.w3.org/1999/xhtml");
+    xml.att("xmlns:epub", "http://www.idpf.org/2007/ops");
+    xml.att("lang", epub.metadata.language);
+    let head = xml.ele("head");
+    let meta = head.ele("meta");
+    meta.att("charset", "UTF-8");
+    let title = head.ele("title");
+    title.txt("Navigation");
+    let style = head.ele("link");
+    style.att("type", "text/css");
+    style.att("href", "css/style.css");
+    style.att("rel", "stylesheet");
+    let body = xml.ele("body");
+    let nav = body.ele("nav");
+    nav.att("epub:type", "toc");
+    nav.att("id", "toc");
+    let h1 = nav.ele("h1");
+    h1.txt("Table of Contents");
+    let ol = nav.ele("ol");
+    if (epub.table != undefined) {
+        for (let item of epub.table) {
+            let li = ol.ele("li");
+            let a = li.ele("a");
+            a.att("href", item.href);
+            a.txt(item.title);
+        }
+    }
+    // write file
+    const file_path = epub.working_folder + "/item/" + file_name;
+    fs.writeFileSync(file_path, xml.end({ pretty: true }));
+}
+
+/**
+ * create standard.opf
+ * @param {Epub} epub
+ */
+async function create_standard_opf(epub) {
     // const file_name = "standard.opf";
     // make xml text
     let xml = builder.create("package");
@@ -238,6 +351,6 @@ function create_standard_opf(epub) {
         }
     }
 
-    const path = epub.working_folder + "/item/standard.opf";
-    fs.writeFileSync(path, xml.end({ pretty: true }));
+    const file_path = epub.working_folder + "/item/standard.opf";
+    fs.writeFileSync(file_path, xml.end({ pretty: true }));
 }
